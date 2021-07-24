6411ca04c75ed383dace0419f1bb9a7e4130b929
kakarotocad <sergiocad2018@gmail.com>
2021-07-24 16:39:49 -0600

    Working with arrays added groupBy

--- src/main/kotlin/one/digitalinnovation/collections/TestList.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TestList.kt
@@ -1,9 +1,9 @@
 package one.digitalinnovation.collections
 
 fun main() {
-    val func1 = Funcionario("Jonas",2500.0)
-    val func2 = Funcionario("Edward",3700.0)
-    val func3 = Funcionario("Michael",2950.0)
+    val func1 = Funcionario("Jonas",2500.0,"CLT")
+    val func2 = Funcionario("Edward",3700.0, "PJ")
+    val func3 = Funcionario("Michael",2950.0, "CLT")
 
     val employees = listOf(func1,func2,func3)
 
@@ -12,17 +12,25 @@ fun main() {
     println(employees.find { it.name == "Jonas" })
 
     println("-------------------")
-    employees.sortedBy { it.salary }.forEach { println(it) }
+    employees
+        .sortedBy{ it.salary }
+        .forEach { println(it) }
+
+    println("-------------------")
+    employees
+        .groupBy{ it.contractType }
+        .forEach { println(it) }
 
 }
 
 data class Funcionario(
     val name:String,
-    val salary: Double
+    val salary: Double,
+    val contractType: String
 ){
     override fun toString(): String =
         """
             Nome:       $name
-            Salario:    $salary    
+            Salario:    $salary
         """.trimIndent()
 }
